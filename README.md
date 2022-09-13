4944cddb9f3b9e6593910847fa9128bc26350adf
Oscar Lastra <oscar.glastra@gmail.com>
2022-09-12 23:37:41 -0500

    chore: Add README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ruby Snake 🐍
+
+Snake game inspired by [nsnake](https://github.com/alexdantas/nSnake) made with Ruby.
--- test/tc_game.rb
+++ test/tc_game.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 require_relative '../lib/game'
+require_relative '../lib/snake'
 require 'test/unit'
 
 # Tests Game Class.
 class TestGame < Test::Unit::TestCase
   def setup
-    @test_game1 = Game.new(10, 3)
-    @test_game2 = Game.new(8, 3)
+    @snake1 = Snake.new(2, 5)
+    @test_game1 = Game.new(10, 3, @snake)
+    @test_game2 = Game.new(8, 3, @snake)
   end
 
   def test_make_2d_array_with_dimensions
